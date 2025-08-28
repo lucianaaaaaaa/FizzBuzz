98b62afd6fb516f03a8432907f38acb21d78fa01
lucianaaaaaaa <luciana.leano@ucb.edu.bo>
2025-08-27 23:23:16 -0400

    feat: generar toda la secuencia

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import convertirFizzBuzz from "./fizzbuzz.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
-
-form.addEventListener("submit", (event) => {
+document.getElementById("sumar-form").addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const inputNumber = parseInt(document.getElementById("primer-numero").value, 10);
+  const resultDiv = document.getElementById("resultado-div");
+  const sequence = [];
+  for (let i = 1; i <= inputNumber; i++) {
+    sequence.push(convertirFizzBuzz(i));
+  }
+  resultDiv.textContent = sequence.join(", ");
 });
+
